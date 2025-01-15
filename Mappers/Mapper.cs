e022d831414372cc4e13198da0d31f19147adb85
Siya10 <leyanesiyabonga00@gmail.com>
2025-01-15 13:55:24 +0200

    Added an abstract mapper class and implemented mapper 000

---
+++ Mappers/Mapper.cs
@@ -0,0 +1,7 @@
+namespace _6502Clone
+{
+    abstract class Mapper
+    {
+        public abstract ushort translateAddr(ushort cpuAddr);
+    }
+}
---
+++ Mappers/_000.cs
@@ -0,0 +1,10 @@
+namespace _6502Clone
+{
+    class Mappper000: Mapper
+    {
+        public override ushort translateAddr(ushort cpuAddr)
+        {
+            return cpuAddr;
+        }
+    }
+}
